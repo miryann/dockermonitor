08b15f75f8949f986b37a8e591b0483366e6c089
miryann <32993970+miryann@users.noreply.github.com>
2020-01-10 13:19:29 +1100

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -6,7 +6,17 @@
 # Last Update: 10/Jan/20
 
 now=$(date +"%T")
+ 
 	echo "$now: Pulse campaign successfully loaded"|tee -a campaign.log|wall -n
+	echo "$now: Configuring dockermonitor service"|tee -a campaign.log|wall -n
+	mkdir /opt/dockermonitor
+	cp dockermonitor.sh /opt/dockermonitor
+	chmod +x /opt/dockermonitor/dockermonitor.sh
+	cp dockermonitor.service /etc/systemd/system
+	chmod 644 /etc/systemd/system/dockermonitor.service
+	systemctl daemon-reload
+	systemctl enable dockermonitor.service
+	
 sleep 2
 	echo "$now: Downloading docker image alpine/alpine"|tee -a campaign.log|wall -n
 	echo "docker pull alpine"|tee -a campaign.log|wall -n
@@ -29,7 +39,7 @@ sleep 2
 	docker run -td --name ubuntu ubuntu|tee -a campaign.log|wall -n
 	echo "$now: Docker container ubuntu ubuntu running"|tee -a campaign.log|wall -n
  ./runonce.sh
-	echo "$now: Installing and enabling dockermonitor service"|tee -a campaign.log|wall -n
+	echo "$now: Starting dockermonitor service"|tee -a campaign.log|wall -n
 service dockermonitor start |wall -n
 	echo "$now: dockermonitor service started"|tee -a campaign.log|wall -n
  	echo "$now: Pulse campaign successfully completed"|tee -a campaign.log|wall -n
