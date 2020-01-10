f584f966de468fa5f84f81ccc2795d98c8d9cd14
miryann <32993970+miryann@users.noreply.github.com>
2020-01-10 14:09:08 +1100

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -5,6 +5,10 @@
 # Version: 1.1
 # Last Update: 10/Jan/20
 
+AGENTBINPATH="/opt/vmware/iotc-agent/bin/"
+AGENTDATAPATH="/opt/vmware/iotc-agent/data/data/"
+DEVICEID=$(${AGENTBINPATH}DefaultClient get-devices | head -n2 | awk 'NR>1 {split($0,a); print a[1]}')
+TEMPLATE=shelf_container
 now=$(date +"%T")
 	cd /tmp/dockermonitor/
  	chmod 777 *.sh
@@ -28,7 +32,26 @@ docker pull alpine |wall -n
 sleep 2
 	docker run -td --name alpine alpine|tee -a campaign.log|wall -n
 	echo "$now: Docker container alpine alpine running"|tee -a campaign.log|wall -n
-sh runonce.sh|tee -a campaign.log|wall -n
+	
+################################################################################
+## Check to see if running Containers are registered in Pulse, if not, Register
+################################################################################
+for i in $(docker ps -a --format "{{.Names}}"); do
+        if ls $AGENTDATAPATH | grep "$i"; then
+            echo "Container" ${i} "is registered"
+        else
+            echo "Container is not registered"
+            (sudo ${AGENTBINPATH}DefaultClient enroll-device --template=$TEMPLATE --name=T-DockerContainer-${i} --parent-id=$DEVICEID) | grep "Device Id:" | head -1 > ${AGENTDATAPATH}${i}.container
+            RESULT=$?
+                if [ $RESULT -eq 0 ]; then
+                    echo "Container " ${i} " registered successfully" 
+                else
+                    echo "Unable to register Container " ${i}  
+                    exit 1
+                fi
+        fi
+done
+
 sleep 2
 	echo "$now: Downloading docker image ubuntu/ubuntu"|tee -a campaign.log|wall -n
 	echo "docker pull ubuntu"|tee -a campaign.log|wall -n
@@ -39,7 +62,27 @@ docker pull ubuntu |wall -n
 sleep 2
 	docker run -td --name ubuntu ubuntu|tee -a campaign.log|wall -n
 	echo "$now: Docker container ubuntu ubuntu running"|tee -a campaign.log|wall -n
-sh runonce.sh|tee -a campaign.log|wall -n	echo "$now: Starting dockermonitor service"|tee -a campaign.log|wall -n
+	
+################################################################################
+## Check to see if running Containers are registered in Pulse, if not, Register
+################################################################################
+for i in $(docker ps -a --format "{{.Names}}"); do
+        if ls $AGENTDATAPATH | grep "$i"; then
+            echo "Container" ${i} "is registered"
+        else
+            echo "Container is not registered"
+            (sudo ${AGENTBINPATH}DefaultClient enroll-device --template=$TEMPLATE --name=T-DockerContainer-${i} --parent-id=$DEVICEID) | grep "Device Id:" | head -1 > ${AGENTDATAPATH}${i}.container
+            RESULT=$?
+                if [ $RESULT -eq 0 ]; then
+                    echo "Container " ${i} " registered successfully" 
+                else
+                    echo "Unable to register Container " ${i}  
+                    exit 1
+                fi
+        fi
+done
+
+echo "$now: Starting dockermonitor service"|tee -a campaign.log|wall -n
 service dockermonitor start |wall -n
 	echo "$now: dockermonitor service started"|tee -a campaign.log|wall -n
  	echo "$now: Pulse campaign successfully completed"|tee -a campaign.log|wall -n
